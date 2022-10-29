ea3d8fac96d938dbba1608cc04b1fedbc77fcb17
LTDigor <ovd2001@mail.ru>
2022-10-29 23:10:55 +0300

    add streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,69 @@
+import os
+
+import cv2
+import numpy as np
+import pandas
 import streamlit as st
 from PIL import Image
 
 from languages import Languages
 
 
-def get_text(img):
-    return ["text"], [0.6]
+@st.cache(suppress_st_warning=True)
+def load_data():
+    return Image.open('resources/img.png')
+
+
+def get_text(img, lang):
+    # x, y: left
+    text = [[[571.0, 192.0], [929.0, 93.0], [981.0, 287.0], [623.0, 386.0]],
+            [[996.0, 218.0], [1385.0, 227.0], [1382.0, 343.0], [993.0, 333.0]],
+            [[962.0, 830.0], [1242.0, 979.0], [1192.0, 1074.0], [912.0, 925.0]]]
+
+    name = ['ёть', 'ИЛи', 'o']
+
+    prob = [0.6760122179985046, 0.6730091571807861, 0.8272780776023865]
+    return text, name, prob
+
+
+def write_bbox(img, bbox, color=(0, 255, 255), thickness=4):
+    return cv2.rectangle(img, (int(bbox[0][0]), int(bbox[0][1])), (int(bbox[2][0]), int(bbox[2][1])), color, thickness)
+
+
+def write_bboxes(img, bboxes):
+    res = img.copy()
+    for i in range(len(bboxes)):
+        res = write_bbox(res, bboxes[i])
+    return res
 
 
 def main():
-    st.title("Цифровой прорыв, ЮФО. Команда \"щшгненгшщ\"")
-    st.subheader("Text recognition")
+    st.title("Цифровой прорыв. Команда \"щшгненгшщ\"")
 
-    lang = st.sidebar.radio("Select language", list(Languages), format_func=lambda o: o.full_name)
+    img = load_data()
     file = st.file_uploader("Load your picture", type=['png', 'jpg'])
-    if file is not None:
-        img = Image.open(file)
-        # тут подаем на нейронку
-        # рисуем bbox
-        # выводим картинку с bbox
-        st.image(img)
+    lang = st.sidebar.radio("Select language", list(Languages), format_func=lambda o: o.full_name)
 
-#         рисуем скор
+    # load default image
+    img = load_data() if file is None else Image.open(file)
+    img_arr = np.array(img)
+
+    # use neuro network
+    bboxes, texts, probs = get_text(img_arr, lang)
+
+    # draw bboxes
+    img_bboxes = write_bboxes(img_arr, bboxes)
+
+    # show images
+    col1, col2 = st.columns(2)
+    with col1:
+        st.text("Your uploaded photo:")
+        st.image(img)
+    with col2:
+        st.text("Text in the photo:")
+        st.image(img_bboxes)
+
+    st.table(pandas.DataFrame(list(zip(texts, probs)), columns=['Text', 'Probability']))
 
 
 if __name__ == '__main__':
